0bd7917087cac18aa123ebfa50a413d423705bdc
Yasu <yassantune@gmail.com>
2018-04-16 16:12:02 +0000

    step 1 commit

---
+++ 01.https-request/https-request.js
@@ -0,0 +1,27 @@
+function getAndPrintHTMLChunks () {
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* step 1 */
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('\n', data);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+
+getAndPrintHTMLChunks();
+
+
