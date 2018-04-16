80c2da315759d237f97c1506a773137df1c43ef6
Yasu <yassantune@gmail.com>
2018-04-16 18:09:44 +0000

    step 5

---
+++ 01.https-request/http-functions.js
@@ -0,0 +1,20 @@
+module.exports = function getHTML (options, callback) {
+  var https = require('https');
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      var html = [];
+      for (val of chunk) {
+        html += val;
+      }
+      printHTML(html);
+    });
+  });
+};
+
+function printHTML (html) {
+  console.log(html);
+}
---
+++ 01.https-request/test.js
@@ -0,0 +1,8 @@
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+getHTML(requestOptions);
